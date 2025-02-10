32ce75bd366a7a50c4164608ac78a6802e6385a7
fateme529 <fdamavandi529@gmail.com>
2025-02-10 22:16:28 +0330

    v2.4.0

---
+++ cooler_cpu.sql
@@ -0,0 +1,15 @@
+USE payga;
+
+CREATE TABLE cc_socket_compatible_with (
+    cooler_id INT NOT NULL,
+    cpu_id INT NOT NULL,
+    PRIMARY KEY (cooler_id, cpu_id),
+    CONSTRAINT fk_cooler FOREIGN KEY (cooler_id) 
+        REFERENCES COOLER (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT fk_cpu FOREIGN KEY (cpu_id) 
+        REFERENCES CPU (id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
