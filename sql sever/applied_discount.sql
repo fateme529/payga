862c5f12c4b10163632eacf92767a6b76739f15c
fateme529 <fdamavandi529@gmail.com>
2025-02-12 18:20:35 +0330

    v8.9.0

---
+++ sql sever/applied_discount.sql
@@ -0,0 +1,86 @@
+ALTER VIEW Cart_Total_Discount AS
+SELECT
+    added.cart_number,
+    added.locked_number,
+ 
+    ct.cart_total, 
+    STUFF((SELECT ', ' + ap.app_code
+           FROM applied_to ap 
+           WHERE ap.cart_number = added.cart_number 
+             AND ap.locked_number = added.locked_number
+           FOR XML PATH('')), 1, 2, '') AS discount_codes, 
+
+ 
+    SUM(CASE 
+        WHEN ap.app_code IS NOT NULL 
+            AND dc.expiration_date > GETDATE() 
+        THEN 
+            CASE
+                WHEN dc.amount < 100 THEN 
+               
+                    CASE 
+                        WHEN (ct.cart_total * (dc.amount / 100.0)) < dc.dis_limit THEN 
+                            (ct.cart_total * (dc.amount / 100.0))
+                        ELSE 
+                            dc.dis_limit 
+                    END
+                ELSE 
+              
+                    CASE 
+                        WHEN dc.amount < dc.dis_limit THEN dc.amount
+                        ELSE dc.dis_limit 
+                    END
+            END
+        ELSE 0
+    END) AS total_discount_amount, 
+
+ 
+    ct.cart_total - 
+        SUM(CASE 
+            WHEN ap.app_code IS NOT NULL 
+                AND dc.expiration_date > GETDATE() 
+            THEN 
+                CASE
+                    WHEN dc.amount < 100 THEN
+                        
+                        CASE 
+                            WHEN (ct.cart_total * (dc.amount / 100.0)) < dc.dis_limit THEN 
+                                (ct.cart_total * (dc.amount / 100.0))
+                            ELSE 
+                                dc.dis_limit 
+                        END 
+                    ELSE
+                    
+                        CASE 
+                            WHEN dc.amount < dc.dis_limit THEN dc.amount
+                            ELSE dc.dis_limit 
+                        END  
+                END
+            ELSE 0
+        END) AS final_total  
+
+FROM
+    added_to added
+JOIN
+    product p ON added.product_id = p.id
+LEFT JOIN
+    applied_to ap ON added.cart_number = ap.cart_number 
+    AND added.locked_number = ap.locked_number
+LEFT JOIN
+    discount_code dc ON ap.app_code = dc.dis_code  
+JOIN 
+ 
+    (SELECT 
+        added.cart_number,
+        added.locked_number,
+        SUM(added.quantity * p.current_price) AS cart_total
+     FROM 
+        added_to added
+     JOIN 
+        product p ON added.product_id = p.id
+     GROUP BY 
+        added.cart_number, added.locked_number) ct 
+    ON added.cart_number = ct.cart_number 
+    AND added.locked_number = ct.locked_number
+GROUP BY
+    added.cart_number, added.locked_number, ct.cart_total;
