74d4dcf32a58b8fa424c4a9c710f0d3a0f7eaaec
fateme529 <fdamavandi529@gmail.com>
2025-02-14 02:33:23 +0330

    v 10.5.0

---
+++ sql sever/trigger_for_validation_bank_transaction.sql
@@ -0,0 +1,17 @@
+CREATE TRIGGER trg_CheckBankTransaction
+ON bank_transaction
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    IF EXISTS (
+        SELECT 1 
+        FROM inserted i
+        LEFT JOIN wallet_transaction wt ON i.tracking_code = wt.tracking_code
+        WHERE wt.tracking_code IS NULL
+    )
+    BEGIN
+        RAISERROR('Tracking code not found in wallet_transaction. Insert operation is cancelled.', 16, 1);
+        ROLLBACK TRANSACTION;
+    END
+END;
