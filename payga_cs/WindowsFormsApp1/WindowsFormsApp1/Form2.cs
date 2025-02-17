4730996d4a2fe5c3e275648274876626e471d5b9
fateme529 <fdamavandi529@gmail.com>
2025-02-17 04:48:49 +0330

    v11.2.3

---
+++ payga_cs/WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+
+        private void sign_Click(object sender, EventArgs e)
+        {
+            Form1 loginform = new Form1();
+            loginform.Show();
+            this.Hide();
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
