d795dd38c8db1d4d63b1bdcd95cd9d90779612f7
fateme529 <fdamavandi529@gmail.com>
2025-02-21 15:03:50 +0330

    v11.7.8

--- payga_cs/WindowsFormsApp1/WindowsFormsApp1/cart.Designer.cs
+++ payga_cs/WindowsFormsApp1/WindowsFormsApp1/cart.Designer.cs
@@ -41,11 +41,21 @@ namespace WindowsFormsApp1
             this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
             this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
             this.flowLayoutPanel5 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.flowLayoutPanel4.SuspendLayout();
+            this.flowLayoutPanel5.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -108,6 +118,7 @@ namespace WindowsFormsApp1
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(113)))), ((int)(((byte)(73)))));
+            this.flowLayoutPanel1.Controls.Add(this.label2);
             this.flowLayoutPanel1.Location = new System.Drawing.Point(859, 340);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(176, 179);
@@ -116,6 +127,7 @@ namespace WindowsFormsApp1
             // flowLayoutPanel2
             // 
             this.flowLayoutPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(113)))), ((int)(((byte)(73)))));
+            this.flowLayoutPanel2.Controls.Add(this.label3);
             this.flowLayoutPanel2.Location = new System.Drawing.Point(656, 340);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
             this.flowLayoutPanel2.Size = new System.Drawing.Size(176, 179);
@@ -124,6 +136,7 @@ namespace WindowsFormsApp1
             // flowLayoutPanel3
             // 
             this.flowLayoutPanel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(113)))), ((int)(((byte)(73)))));
+            this.flowLayoutPanel3.Controls.Add(this.label4);
             this.flowLayoutPanel3.Location = new System.Drawing.Point(441, 340);
             this.flowLayoutPanel3.Name = "flowLayoutPanel3";
             this.flowLayoutPanel3.Size = new System.Drawing.Size(176, 179);
@@ -132,6 +145,7 @@ namespace WindowsFormsApp1
             // flowLayoutPanel4
             // 
             this.flowLayoutPanel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(113)))), ((int)(((byte)(73)))));
+            this.flowLayoutPanel4.Controls.Add(this.label5);
             this.flowLayoutPanel4.Location = new System.Drawing.Point(238, 340);
             this.flowLayoutPanel4.Name = "flowLayoutPanel4";
             this.flowLayoutPanel4.Size = new System.Drawing.Size(176, 179);
@@ -140,10 +154,63 @@ namespace WindowsFormsApp1
             // flowLayoutPanel5
             // 
             this.flowLayoutPanel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(113)))), ((int)(((byte)(73)))));
+            this.flowLayoutPanel5.Controls.Add(this.label6);
             this.flowLayoutPanel5.Location = new System.Drawing.Point(36, 340);
             this.flowLayoutPanel5.Name = "flowLayoutPanel5";
             this.flowLayoutPanel5.Size = new System.Drawing.Size(176, 179);
             this.flowLayoutPanel5.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 29);
+            this.label2.TabIndex = 0;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 29);
+            this.label3.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 29);
+            this.label4.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(3, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 29);
+            this.label5.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(3, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 29);
+            this.label6.TabIndex = 3;
             // 
             // cart
             // 
@@ -170,6 +237,16 @@ namespace WindowsFormsApp1
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            this.flowLayoutPanel4.ResumeLayout(false);
+            this.flowLayoutPanel4.PerformLayout();
+            this.flowLayoutPanel5.ResumeLayout(false);
+            this.flowLayoutPanel5.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -188,5 +265,10 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
--- payga_cs/WindowsFormsApp1/WindowsFormsApp1/cart.cs
+++ payga_cs/WindowsFormsApp1/WindowsFormsApp1/cart.cs
@@ -19,16 +19,94 @@ namespace WindowsFormsApp1
         }
         public void LoadUserData3(int userid)
         {
+            using (var db = new db_1Entities())
+            {
+                var isVip = db.vip_client.Any(vc => vc.id == userid);
 
+                // چک کردن سبد خرید اول
+                var cart1 = db.shopping_cart.FirstOrDefault(sc => sc.number == (userid * 100 + 1));
+                if (cart1 != null)
+                {
+                    label2.Text = $" {cart1.status}";
+                }
+                else
+                {
+                    label2.Text = "سبد خرید اول موجود نیست";
+                }
 
+                // چک کردن سبدهای بعدی از 02 تا 05
+                for (int i = 2; i <= 5; i++)
+                {
+                    int cartId = userid * 100 + i;
+                    var cart = db.shopping_cart.FirstOrDefault(sc => sc.number == cartId);
 
-           
-
+                    if (cart != null)
+                    {
+                       
+                        if (isVip)
+                        {
+                            switch (i)
+                            {
+                                case 2:
+                                    label3.Text = $" {cart.status}";
+                                    break;
+                                case 3:
+                                    label4.Text = $" {cart.status}";
+                                    break;
+                                case 4:
+                                    label5.Text = $" {cart.status}";
+                                    break;
+                                case 5:
+                                    label6.Text = $" {cart.status}";
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            // اگر کاربر وی آی پی نباشد
+                            switch (i)
+                            {
+                                case 2:
+                                    label3.Text = "این سبد فقط برای کاربران وی آی پی است";
+                                    break;
+                                case 3:
+                                    label4.Text = "این سبد فقط برای کاربران وی آی پی است";
+                                    break;
+                                case 4:
+                                    label5.Text = "این سبد فقط برای کاربران وی آی پی است";
+                                    break;
+                                case 5:
+                                    label6.Text = "این سبد فقط برای کاربران وی آی پی است";
+                                    break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // اگر سبد خرید موجود نباشد
+                        switch (i)
+                        {
+                            case 2:
+                                label3.Text = "سبد خرید موجود نیست";
+                                break;
+                            case 3:
+                                label4.Text = "سبد خرید موجود نیست";
+                                break;
+                            case 4:
+                                label5.Text = "سبد خرید موجود نیست";
+                                break;
+                            case 5:
+                                label6.Text = "سبد خرید موجود نیست";
+                                break;
+                        }
+                    }
+                }
+            }
         }
 
 
-         
-            private void label1_Click(object sender, EventArgs e)
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
@@ -39,6 +117,11 @@ namespace WindowsFormsApp1
         }
 
         private void cart_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
         {
 
         }
