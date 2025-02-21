44ef8572d4c00bedf2f2cf6718092f7d088e6d4c
fateme529 <fdamavandi529@gmail.com>
2025-02-22 00:17:50 +0330

    v11.8.3

--- payga_cs/WindowsFormsApp1/WindowsFormsApp1/overviwe.Designer.cs
+++ payga_cs/WindowsFormsApp1/WindowsFormsApp1/overviwe.Designer.cs
@@ -40,6 +40,9 @@ namespace WindowsFormsApp1
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -49,35 +52,36 @@ namespace WindowsFormsApp1
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(111, 86);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(111, 88);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(564, 67);
             this.panel1.TabIndex = 0;
             // 
             // panel2
             // 
-            this.panel2.Location = new System.Drawing.Point(111, 182);
+            this.panel2.Location = new System.Drawing.Point(111, 176);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(564, 67);
             this.panel2.TabIndex = 1;
             // 
             // panel3
             // 
-            this.panel3.Location = new System.Drawing.Point(111, 274);
+            this.panel3.Location = new System.Drawing.Point(111, 272);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(564, 67);
             this.panel3.TabIndex = 2;
             // 
             // panel4
             // 
-            this.panel4.Location = new System.Drawing.Point(111, 371);
+            this.panel4.Location = new System.Drawing.Point(111, 379);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(564, 67);
             this.panel4.TabIndex = 2;
             // 
             // panel5
             // 
-            this.panel5.Location = new System.Drawing.Point(111, 466);
+            this.panel5.Location = new System.Drawing.Point(111, 473);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(564, 67);
             this.panel5.TabIndex = 2;
@@ -85,7 +89,7 @@ namespace WindowsFormsApp1
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(681, 162);
+            this.pictureBox1.Location = new System.Drawing.Point(681, 157);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(124, 98);
             this.pictureBox1.TabIndex = 3;
@@ -104,9 +108,9 @@ namespace WindowsFormsApp1
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(681, 261);
+            this.pictureBox3.Location = new System.Drawing.Point(681, 250);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(139, 110);
+            this.pictureBox3.Size = new System.Drawing.Size(139, 121);
             this.pictureBox3.TabIndex = 5;
             this.pictureBox3.TabStop = false;
             // 
@@ -122,17 +126,38 @@ namespace WindowsFormsApp1
             // pictureBox5
             // 
             this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
-            this.pictureBox5.Location = new System.Drawing.Point(681, 452);
+            this.pictureBox5.Location = new System.Drawing.Point(681, 457);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(113, 119);
+            this.pictureBox5.Size = new System.Drawing.Size(113, 114);
             this.pictureBox5.TabIndex = 7;
             this.pictureBox5.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(317, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "5 recent purchases";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 0;
             // 
             // overviwe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(113)))), ((int)(((byte)(73)))));
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
@@ -146,12 +171,15 @@ namespace WindowsFormsApp1
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "overviwe";
             this.Size = new System.Drawing.Size(797, 571);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -167,5 +195,7 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- payga_cs/WindowsFormsApp1/WindowsFormsApp1/overviwe.cs
+++ payga_cs/WindowsFormsApp1/WindowsFormsApp1/overviwe.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
@@ -17,14 +12,66 @@ namespace WindowsFormsApp1
             InitializeComponent();
             LoadUserData4(id);
         }
+
         public void LoadUserData4(int userid)
         {
+            using (var db = new db_1Entities())
+            {
+           
+                var lastTransaction = db.vw_TransactionDetails
+                    .Where(t => t.id == userid)
+                    .OrderByDescending(t => t.timestamp) 
+                    .FirstOrDefault();
 
+                if (lastTransaction != null)
+                {
+                    
+                    var cartNumber = lastTransaction.cart_number;
+                    var lockedNumber = lastTransaction.locked_number;
+
+                  
+                    var products = db.added_to
+                        .Where(a => a.cart_number == cartNumber && a.locked_number == lockedNumber)
+                        .Select(a => a.product_id)  
+                        .ToList();
+
+                    if (products.Any())
+                    {
+                        
+                        var productDetails = db.viewCart_Total_Discount
+                            .Where(v => products.Contains(v.product_id)) 
+                            .ToList();
+
+                      
+                        StringBuilder sb = new StringBuilder();
+                        foreach (var product in productDetails)
+                        {
+                            sb.AppendLine($"Product: {product.product_name}, Price: {product.product_price}, Discount: {product.discount_value}");
+                        }
+
+                        
+                        label2.Text = sb.ToString();
+                    }
+                    else
+                    {
+                        label2.Text = "محصولی در سبد خرید یافت نشد";
+                    }
+                }
+                else
+                {
+                    label2.Text = "هیچ تراکنشی برای این کاربر یافت نشد";
+                }
+            }
         }
 
         private void pictureBox2_Click(object sender, EventArgs e)
         {
+           
+        }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
         }
     }
 }
