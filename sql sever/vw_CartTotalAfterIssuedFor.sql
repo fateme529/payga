1276756f99e473de3a722d3db1d88041f50dacbc
fateme529 <fdamavandi529@gmail.com>
2025-02-24 13:26:29 +0330

    v11.9.4

---
+++ sql sever/vw_CartTotalAfterIssuedFor.sql
@@ -0,0 +1,15 @@
+
+
+ALTER VIEW [dbo].[vw_CartTotalAfterIssuedFor]
+AS
+SELECT 
+    ctd.cart_number,
+    ctd.locked_number,
+    ctd.final_total,
+    t.timestamp 
+FROM dbo.Cart_Total_Discount ctd
+JOIN dbo.issued_for iss ON iss.cart_number = ctd.cart_number
+    AND iss.locked_number = ctd.locked_number
+JOIN dbo.transactions t ON t.tracking_code = iss.tracking_code
+WHERE t.status = 'successful';
+GO
